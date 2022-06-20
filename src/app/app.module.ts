98e50b42042d2c8fd126ba2a17f96105b0fb1998
Vicente <vicenterg9@gmail.com>
2022-06-20 23:37:14 +0200

    feat: primeng

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
 
+import { ButtonModule } from 'primeng/button';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule,
+    CoreModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
