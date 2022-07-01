da4297816efddf9757de5dd92690eb4f70f31283
vicenterg9 <vicenterg9@gmail.com>
2022-07-01 13:12:03 +0200

    feat: form film done

--- src/app/shared/components/films/films.list.ts
+++ src/app/shared/components/films/films.list.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ConfirmationService } from 'primeng/api';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-films-list',
